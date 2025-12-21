19a9779f7956d0fbf133c6a95a21fc6ea03a5342
Steven Simms <simmsstev@gmail.com>
2025-12-20 18:27:23 -0800

    Shell in rust stage 4

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,15 @@ fn main() {
 fn shell_prompt() {
     print!("$ ");
     io::stdout().flush().unwrap();
+    println!("{}", read_input());
+}
+
+fn read_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect(": command not found");
+    let trimmed_input = input.trim();
+    let cmd_not_found = format!("{}: command not found", trimmed_input);
+    cmd_not_found
 }
